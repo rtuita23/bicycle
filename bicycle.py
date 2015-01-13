3539bcfc1d913c55d7d5ffbd89f50a326eeb6b35
rtuita23 <rktuita23@hotmail.com>
2015-01-13 22:37:44 +0000

    v2

--- bicycle.py
+++ bicycle.py
@@ -1,35 +1,36 @@
-class Bicycle:
-    def __init__(self, modelName, weight, cost):
+class Bicycle:    # Create Bicycle class 
+    def __init__(self, modelName, weight, cost):    # Constructor w/ 3 parameters
         self.modelName = modelName
         self.weight = weight
         self.cost = cost
         
-    def __str__(self):
+    def __str__(self):    # Return the parameters for print
         return self.modelName + ' ' + self.weight + ' ' + str(self.cost)
     
-class Shop:
-    stock = ['a', 'b', 'c', 'd', 'e', 'f']
-    def __init__(self, name):
+class Shop(Bicycle):    # Create a Bicycle Shop class which derives from Bicycle Class
+    def __init__(self, name, modelName, weight, cost):    # Constructor w/ 4 parameters
+        Bicycle.__init__(self, modelName, weight, cost)    # Inherit inventory from Bicycle Class
         self.name = name
         
-    def __str__(self):
-        return self.name
+    def __str__(self):    # Return the parameters for print
+        return self.name + ' ' + self.modelName + ' ' + self.weight + ' ' + self.cost
     
-    def sell(self):
+    def price(self):    # Compute sales price per bike
         selling_price = 0
         self.selling_price = self.cost + (self.cost*.20)
         return self.selling_price 
     
-    def profit(self):
-        return sell() - self.cost
+    def profit(self):    # Compute profit margin for all sales
+        return price() - self.cost
     
-class Customer:
+class Customer:    # Create a Customer class
     def __init__(self, name, fund):
         self.name = name
         self.fund = fund
     
     def __str__(self):
         return self.name + ' ' + str(self.fund)
+    
 
 cus1 = Customer('Paul', 200)
 cus2 = Customer('Peter', 500)
